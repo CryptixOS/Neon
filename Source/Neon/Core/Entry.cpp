164f834895987b31e7ddf8c769d49e20e9f1a119
V1tr10l7 <vitriol1744@gmail.com>
2025-09-08 22:54:22 +0200

    [refactor]: change return type of NeonMain to ErrorOr<void>

--- Source/Neon/Core/Entry.cpp
+++ Source/Neon/Core/Entry.cpp
@@ -5,19 +5,23 @@
  * SPDX-License-Identifier: GPL-3
  */
 #include <Prism/Containers/Vector.hpp>
+#include <Prism/Core/Error.hpp>
 #include <Prism/Core/Types.hpp>
 #include <Prism/String/StringView.hpp>
 
 using namespace Prism;
 
-extern int NeonMain(const Vector<StringView>& argv,
-                    const Vector<StringView>& envp);
-int        main(int argc, char** argv, char** envp)
+extern ErrorOr<void> NeonMain(const Vector<StringView>& argv,
+                              const Vector<StringView>& envp);
+int                  main(int argc, char** argv, char** envp)
 {
     Vector<StringView> argArr;
     Vector<StringView> envArr;
     for (isize i = 0; i < argc && argv[i]; i++) argArr.EmplaceBack(argv[i]);
     for (usize i = 0; envp[i]; i++) envArr.EmplaceBack(envp[i]);
 
-    return NeonMain(argArr, envArr);
+    auto status = NeonMain(argArr, envArr);
+    if (!status) return status.Error();
+
+    return 0;
 }
